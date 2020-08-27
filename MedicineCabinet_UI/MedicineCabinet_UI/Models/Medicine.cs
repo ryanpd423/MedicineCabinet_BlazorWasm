948dcd840d639065a2267abc02e9db8f85ecc278
rpd4g5 <ryanpd423@gmail.com>
2020-08-27 10:45:56 -0500

    now able to post new medicines to medicine API and mongodb it intefaces for

---
+++ MedicineCabinet_UI/MedicineCabinet_UI/Models/Medicine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MedicineCabinet_UI.Models
+{
+    public class Medicine
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Dosage { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Pharmacy { get; set; }
+    }
+}
