440815ec0c2398ebd0c69eb30368c3c2b2b3d7de
rpd4g5 <ryanpd423@gmail.com>
2020-09-24 14:05:50 -0500

    was experimenting with adding drag-and-drop via the JS-Interop features of Blazor and Vanilla JS - it is buggy and itsn't fully fledged out but its a start

--- MedicineCabinet_UI/MedicineCabinet_UI/Program.cs
+++ MedicineCabinet_UI/MedicineCabinet_UI/Program.cs
@@ -1,13 +1,8 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 namespace MedicineCabinet_UI
 {
@@ -18,7 +13,8 @@ namespace MedicineCabinet_UI
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient
+                { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
---
+++ MedicineCabinet_UI/MedicineCabinet_UI/wwwroot/draggable.js
@@ -0,0 +1,18 @@
+﻿window.exampleJsFunctions = {
+
+    onDragStart: function() {
+        event.dataTransfer.setData('text/plain', event.target.id);
+        event.target.style.color = 'green';
+    },
+
+    onDragOver: function() {
+        event.preventDefault();
+    },
+
+    onDrop: function () {
+        var data = event.dataTransfer.getData('text/plain');
+        const draggableElement = document.getElementById(data);
+        event.target.appendChild(draggableElement);
+        event.dataTransfer.clearData();
+    }
+};
